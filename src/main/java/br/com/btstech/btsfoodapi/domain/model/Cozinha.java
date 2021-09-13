1511c6caed54c95c26ab583ae79f0dd085a7faca
CaioBastosImpacta <caio.bastos@aluno.faculdadeimpacta.com.br>
2021-09-13 18:53:00 -0300

    criando as primeiras entidades do btsfood

---
+++ src/main/java/br/com/btstech/btsfoodapi/domain/model/Cozinha.java
@@ -0,0 +1,24 @@
+package br.com.btstech.btsfoodapi.domain.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cozinha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+}
---
+++ src/main/java/br/com/btstech/btsfoodapi/domain/model/Restaurante.java
@@ -0,0 +1,29 @@
+package br.com.btstech.btsfoodapi.domain.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+
+    @Column(name = "taxa_frete")
+    private BigDecimal taxaFrete;
+}
