c0d179d4eb599f3abbeba24bd553841e2c8ff791
CaioBastosImpacta <caio.bastos@aluno.faculdadeimpacta.com.br>
2021-09-13 12:09:24 -0300

    Criando o projeto e o primeiro commit

---
+++ src/main/java/br/com/btstech/btsfoodapi/BtsfoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.btstech.btsfoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtsfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtsfoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/btstech/btsfoodapi/BtsfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.btstech.btsfoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtsfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
